2d4d6da3b79360d8041262aa1577f64e6555170b
Quinton Nistico <qnistico@aol.com>
2021-08-02 05:08:10 -0400

    product images minified

--- src/Product.js
+++ src/Product.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./Sass/Product.scss";
-import StarIcon from '@material-ui/icons/Star';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import StarIcon from "@material-ui/icons/Star";
+import StarHalfIcon from "@material-ui/icons/StarHalf";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 const Product = ({ product, onAddToCart }) => {
-    return (
-        <div className="productcard">
-            <div className="productcard-body">
-            <img src={product.media.source} alt="" />
-<div className="productcard-content">
-                <p className="pcc-name">{product.name}</p>
-                <p className="pcc-description" dangerouslySetInnerHTML={{ __html: product.description}} />
-                <div className="stars">
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarHalfIcon />
-                </div>
-                <p className="pcc-price"><span className="from">from </span>{product.price.formatted_with_symbol}</p>
-                
-                <button  className="add-to-cart" onClick={() => onAddToCart(product.id, 1)}><AddShoppingCartIcon /></button>
+  return (
+    <div className="productcard">
+      <div className="productcard-body">
+        <img src={product.media.source} alt="" />
+        <div className="productcard-content">
+          <p className="pcc-name">{product.name}</p>
+          <p
+            className="pcc-description"
+            dangerouslySetInnerHTML={{ __html: product.description }}
+          />
+          <div className="stars">
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarHalfIcon />
+          </div>
+          <div className="productcard-bottom">
+          <p className="pcc-price">
+            <span className="from">from </span>
+            {product.price.formatted_with_symbol}
+          </p>
 
-                </div>
-            </div>
+          <button
+            className="add-to-cart"
+            onClick={() => onAddToCart(product.id, 1)}
+          >
+            <AddShoppingCartIcon />
+          </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
