86a0547d5a7cb46ac20f56e3592af67f78e35307
Quinton Nistico <qnistico@aol.com>
2021-08-10 01:39:58 -0400

    solved silly error

--- src/CheckoutForm/AddressForm.js
+++ src/CheckoutForm/AddressForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from "react-hook-form";
+import CustomTextField from './CustomTextField';
 
 const AddressForm = () => {
+    const methods = useForm();
     return (
         <div>
-            Address Form
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <CustomTextField required name="firstName" label="First name" />
+                    </Grid>
+                </form>
+            </FormProvider>
         </div>
     )
 }
