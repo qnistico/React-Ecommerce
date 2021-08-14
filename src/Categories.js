aa3581ac8128d33c57847f733780ffcc96e5d7ed
Blake Joynes <blakejoynes@gmail.com>
2021-08-14 08:22:32 -0400

    add logic for getting category for product

--- src/Categories.js
+++ src/Categories.js
@@ -5,16 +5,24 @@ import { useParams } from "react-router";
 const Categories = ({ categories, products, onAddToCart }) => {
   
   const { category_id } = useParams();
-  products
-    .filter((p) => p.category_id === category_id)
-    .map((product) => (
-      <div className="product-card" key={product.id}>
-        <Product product={product} onAddToCart={onAddToCart} />
-      </div>
-    ));
+
+    /**
+     * Filter products that are in a certain category.
+     * @returns {*}
+     */
+  const getProductsByCategory = () => {
+      // each product has array of categories, so you can use this .some() function to loop and check if it contains current id.
+      return products
+          .filter((prd) => prd.categories.some((category) => category.id === category_id));
+  };
+
   console.log(categories);
   return <div>
-    
+      {getProductsByCategory().map((product) => (
+      <div className="product-card" key={product.id}>
+          <Product product={product} onAddToCart={onAddToCart} />
+      </div>
+      ))}
   </div>;
 };
 
